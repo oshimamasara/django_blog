13321b1843ce6003700b60586fe748c6d53dcbdd
YOUR NAME <YOUR@EMAIL.com>
2019-12-31 05:08:12 +0900

    add markdown function

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'slug', 'created_datetime', 'updated_datetime')
     list_display_links = ('id', 'title')
 
-admin.site.register(Post, PostAdmin)
+#admin.site.register(Post, PostAdmin)
+admin.site.register(Post, MarkdownxModelAdmin)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from django.utils.text import slugify
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
-    text = models.TextField()
+    #text = models.TextField()
+    text = MarkdownxField()
     created_datetime = models.DateTimeField(auto_now_add=True)
     updated_datetime = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
 
+    def formatted_markdown(self):
+        return markdownify(self.text)
+
 def save(self, *args, **kwargs):
     self.slug = slugify(self.title)
     super().save(*args, **kwargs)
+
+
